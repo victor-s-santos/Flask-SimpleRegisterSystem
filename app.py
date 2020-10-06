755f7c0bba81d51963925dd3f931203ed63a6f11
Victor Santos Silva <victor.s.silva@unesp.br>
2020-10-05 22:53:32 -0300

    Method == POST

--- app.py
+++ app.py
@@ -7,10 +7,27 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('home.html')
+
 #registration form
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        usuario = request.form.get("usuario")
+        senha = request.form.get("senha")
+        confirma = request.form.get("confirm")
+
+        if senha == confirma:
+            print('É igual')
     return render_template("register.html")
+
+#login form
+@app.route("/login")
+def login():
+    if request.method == "POST":
+        return render_template("login.html")
+    else:
+        return(request.method)
 
 if __name__ == "__main__":
     app.run(debug=True)

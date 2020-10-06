ab4aa712edbb34f9601764b7d9724256f929d811
Victor Santos Silva <victor.s.silva@unesp.br>
2020-10-05 22:24:04 -0300

    creating the registration form

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('home.html')
+#registration form
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
